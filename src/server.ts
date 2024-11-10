37e2dca4de7120f8fa1cb5b2cdd36b4861f86b01
luft <araryap@gmail.com>
2024-11-10 10:24:05 +0700

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import movieRouter from './routes/MovieRoute';
 import reviewRouter from './routes/ReviewRoute';
 import userRouter from './routes/UserRoute';
 import { connectDB } from './config/dbConnection';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/movies', movieRouter);
