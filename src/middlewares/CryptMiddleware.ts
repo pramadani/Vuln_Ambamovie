df2f635b573accc64fabad6e24fe5dce478ca741
luft <araryap@gmail.com>
2024-11-18 23:44:02 +0700

    crypt

--- src/middlewares/CryptMiddleware.ts
+++ src/middlewares/CryptMiddleware.ts
@@ -1,23 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
-import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 export class CryptMiddleware {
-    static hashPassword(req: Request, res: Response, next: NextFunction) {
+    static async hashPassword(req: Request, res: Response, next: NextFunction) {
         try {
             if (req.body.password) {
                 const password = req.body.password;
-                
-                const md5Password = crypto.createHash('md5')
-                    .update(password)
-                    .digest('hex');
 
-                req.body.password = md5Password;
+                // Generate salt untuk bcrypt
+                const saltRounds = 10;
+                const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+                req.body.password = hashedPassword;
             }
             next();
         } catch (error) {
             const err = error as Error;
             res.status(500).json({ message: err.message });
             return;
-        };
+        }
     }
 }
