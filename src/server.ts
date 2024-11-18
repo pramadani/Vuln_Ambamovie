227cda9832d87b023b74ce1f2b8d1e242de03b14
luft <araryap@gmail.com>
2024-11-19 01:40:46 +0700

    UP

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import movieRouter from './routes/MovieRoute';
 import reviewRouter from './routes/ReviewRoute';
 import userRouter from './routes/UserRoute';
 import { connectDB } from './config/dbConnection';
-import cors from 'cors';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -12,14 +14,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routing
 app.use('/movies', movieRouter);
 app.use('/reviews', reviewRouter);
 app.use('/users', userRouter);
 
 const startServer = async () => {
     await connectDB();
-    const PORT = process.env.SERVER_PORT!;
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+    const PORT = 443;
+
+    const sslOptions = {
+        key: fs.readFileSync('ssl/privkey.pem', 'utf8'),
+        cert: fs.readFileSync('ssl/fullchain.pem', 'utf8'),
+    };
+
+    https.createServer(sslOptions, app).listen(PORT, () => {
+        console.log(`Server is running on HTTPS port ${PORT}`);
+    });
 };
 
 startServer();
