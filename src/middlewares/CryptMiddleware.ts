f8dbf604cf096ad3a68b20103d9130397ce322fc
luft <araryap@gmail.com>
2024-11-19 02:01:05 +0700

    up

--- src/middlewares/CryptMiddleware.ts
+++ src/middlewares/CryptMiddleware.ts
@@ -3,14 +3,20 @@ import bcrypt from 'bcrypt';
 
 export class CryptMiddleware {
     static async hashPassword(req: Request, res: Response, next: NextFunction) {
-        if (req.body.password) {
-            const password = req.body.password;
+        try {
+            if (req.body.password) {
+                const password = req.body.password;
 
-            const saltRounds = 10;
-            const hashedPassword = await bcrypt.hash(password, saltRounds);
+                const saltRounds = 10;
+                const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-            req.body.password = hashedPassword;
+                req.body.password = hashedPassword;
+            }
+            next();
+        } catch (error) {
+            const err = error as Error;
+            res.status(500).json({ message: err.message });
+            return;
         }
-        next();
     }
 }
