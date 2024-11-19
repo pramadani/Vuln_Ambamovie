1b95a5acaaf2866d3299521befe0f883932f4c05
luft <araryap@gmail.com>
2024-11-19 10:53:19 +0700

    up

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -2,16 +2,20 @@ import express from 'express';
 import { UserController } from '../controllers/UserController';
 import { SessionMiddleware } from '../middlewares/SessionMiddleware';
 import { CryptMiddleware } from '../middlewares/CryptMiddleware';
+import { UserMiddleware } from '../middlewares/UserMiddleware';
 
 const userRouter = express.Router();
 
 userRouter.post('/register',
     CryptMiddleware.hashPassword,
+    UserMiddleware.validateEmail,
+    UserMiddleware.validatePassword,
     UserController.register
 );
 
 userRouter.post('/login',
     CryptMiddleware.hashPassword,
+    UserMiddleware.validateEmail,
     UserController.login
 );
 
